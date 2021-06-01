5f6a9dd4f84a226a1c6fbde630e6baee6b72efd8
JRcd95 <joanieruchaud@gmail.com>
2021-06-01 21:54:51 +0200

    squelette filtre global fait

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Search\Search;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,11 +18,48 @@ class SearchType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('campusSearch', EntityType::class, [
+                'label' => 'Campus :',
+                'required' => false,
+                'class' => Campus::class,
+                'expanded' => true,
+                'multiple' => true,
+                'attr' => [
+                    'placeholder' => '--Campus--'
+            ]])
             ->add('recherche', TextType::class, [
                 'label' => 'Le nom de la sortie contient :',
                 'required' => false,
                 'attr' => ['placeholder' => 'Search'
-                ]]);
+                ]])
+            ->add('dateDebutSearch', DateType::class,[
+                'label' => 'Entre ',
+                'required' => false,
+                'widget' => 'single_text'
+            ])
+            ->add('dateFinSearch', DateType::class,[
+                'label' => 'et',
+                'required' => false,
+                'widget' => 'single_text'
+            ])
+            ->add('sortieOrganisee', CheckboxType::class,[
+                'label' => 'Sorties dont je suis l\'organisateur/trice',
+                'required' => false
+            ])
+            ->add('sortieInscrit', CheckboxType::class,[
+                'label' => 'Sorties auxquelles je suis inscrit/e',
+                'required' => false
+            ])
+            ->add('sortieNonInscrit', CheckboxType::class,[
+                'label' => 'Sorties auxquelles je ne suis pas inscrit/e',
+                'required' => false
+            ])
+            ->add('sortiePassee', CheckboxType::class,[
+                'label' => 'Sorties passée',
+                'required' => false
+            ])
+
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Search/Search.php
+++ src/Search/Search.php
@@ -1,9 +1,36 @@
 <?php
 namespace App\Search;
 
+use App\Entity\Campus;
+use DateTime;
+
 class Search {
+
+    /**
+     * @var Campus
+     */
+    public $campusSearch = [];
     /**
      * @var string
      */
     public $recherche = '';
+    /**
+     * @var DateTime
+     */
+    public $dateDebutSearch;
+    /**
+     * @var DateTime
+     */
+    public $dateFinSearch;
+
+    public $sortieOrganisee;
+
+    public $sortieInscrit;
+
+    public $sortieNonInscrit;
+
+    public $sortiePassee;
+
+
+
 }
