f2d3570738a690818129f46fc6891be2658b348f
JRcd95 <joanieruchaud@gmail.com>
2021-06-02 11:32:24 +0200

    modif pour tableau des sorties colonne inscrit : non concluant

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\SearchType;
+use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use App\Search\Search;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,14 +15,13 @@ class SortieController extends AbstractController
     /**
      * @Route("/sorties", name="sortie")
      */
-    public function list(SortieRepository $sortieRepository, Search $search): Response
+    public function list(SortieRepository $sortieRepository, Search $search, ParticipantRepository $participantRepository): Response
     {
         $searchForm = $this->createForm(SearchType::class, $search);
-
         $sorties = $sortieRepository->findAll();
         return $this->render('sortie/sortie.html.twig', [
             'searchForm' => $searchForm->createView(),
-            "sorties" => $sorties
+            'sorties' => $sorties
 
         ]);
     }
